d64c3e6e84bdfeb9e650933537901701563bbb86
Ckiner <christiankiner21@icloud.com>
2024-11-01 20:48:36 -0500

    Index view now routes to index.html

--- ocr_nutrition_app/ocr_nutrition_app/NutritionApp/views.py
+++ ocr_nutrition_app/ocr_nutrition_app/NutritionApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello")
+    f = open("ocr_nutrition_app/NutritionApp/frontend/index.html")
+    return HttpResponse(f.read())
