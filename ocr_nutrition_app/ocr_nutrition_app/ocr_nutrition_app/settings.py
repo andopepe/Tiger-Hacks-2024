b3655a0a0dc1abf24fbea15e48ead51eebf2bcfe
Ando Pepe <coolcatando@gmail.com>
2024-11-01 21:29:39 -0500

    added redis to requirements

--- ocr_nutrition_app/ocr_nutrition_app/ocr_nutrition_app/settings.py
+++ ocr_nutrition_app/ocr_nutrition_app/ocr_nutrition_app/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'ocr_nutrition_app.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-}
+# }
 
 CACHES = {
        "default": {
