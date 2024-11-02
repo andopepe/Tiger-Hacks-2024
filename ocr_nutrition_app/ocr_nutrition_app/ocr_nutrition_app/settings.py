cb7c7d0109e767c6245a6d08633c4558261b0e58
Ando Pepe <coolcatando@gmail.com>
2024-11-02 16:03:05 -0500

    tmp fix

--- ocr_nutrition_app/ocr_nutrition_app/ocr_nutrition_app/settings.py
+++ ocr_nutrition_app/ocr_nutrition_app/ocr_nutrition_app/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'ocr_nutrition_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'ocr_nutrition_db',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',  # Default PostgreSQL port
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ocr_nutrition_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
