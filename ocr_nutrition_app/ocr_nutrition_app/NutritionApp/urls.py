8fbe33d125bea863d0d370da78a83d01efc08d9b
Ando Pepe <coolcatando@gmail.com>
2024-11-02 11:36:29 -0500

    back to sqlite3

--- ocr_nutrition_app/ocr_nutrition_app/NutritionApp/urls.py
+++ ocr_nutrition_app/ocr_nutrition_app/NutritionApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="Index"),
-    path("cached", views.cached, name='cached'),
-    path("cacheless", views.cacheless, name='cacheless'),
+    # path("cached", views.cached, name='cached'),
+    # path("cacheless", views.cacheless, name='cacheless'),
     path("upload", views.Upload, name="Upload")
 ]
--- ocr_nutrition_app/ocr_nutrition_app/NutritionApp/views.py
+++ ocr_nutrition_app/ocr_nutrition_app/NutritionApp/views.py
@@ -9,16 +9,16 @@ from django.views.decorators.cache import cache_page
 def index(request):
     return render(request, "NutritionApp/index.html")
 
-@cache_page(30)
-def cached(request):
-    user_model = get_user_model()
-    all_users = user_model.objects.all()
-    return HttpResponse('<html><body><h1>{0} users .. cached</h1></body></html>'.format(len(all_users)))
+# @cache_page(30)
+# def cached(request):
+#     user_model = get_user_model()
+#     all_users = user_model.objects.all()
+#     return HttpResponse('<html><body><h1>{0} users .. cached</h1></body></html>'.format(len(all_users)))
 
-def cacheless(request):
-    user_model = get_user_model()
-    all_users = user_model.objects.all()
-    return HttpResponse('<html><body><h1>{0} users .. cacheless</h1></body></html>'.format(len(all_users)))
+# def cacheless(request):
+#     user_model = get_user_model()
+#     all_users = user_model.objects.all()
+#     return HttpResponse('<html><body><h1>{0} users .. cacheless</h1></body></html>'.format(len(all_users)))
 
 def Upload(request):
     if request.method == 'POST':
